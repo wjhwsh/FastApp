6958ca56c9705ac022b1f54606217f1bef5b7fd3
Jianhua Wang <wjhwsh@gmail.com>
2021-08-12 16:56:45 -0700

    Initial Commit

---
+++ FastAssignment/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FastAssignment
+//
+//  Created by Jianhua Wang on 8/12/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
